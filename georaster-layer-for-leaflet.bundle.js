cc35de66ca4df743238226077462bc185aa64945
Joanna Lin <chiayilin94@gmail.com>
2018-12-02 09:56:02 -0800

    remove console.log disabler since it does not work

--- georaster-layer-for-leaflet.bundle.js
+++ georaster-layer-for-leaflet.bundle.js
@@ -222,9 +222,7 @@ var GeoRasterLayer = L.GridLayer.extend({
         d.style.color = name;
         document.body.appendChild(d);
         return window.getComputedStyle(d).color;
-    },
-
-    console.log: function() {}
+    }
 });
 
 if (typeof module !== "undefined" && typeof module.exports !== "undefined") {
